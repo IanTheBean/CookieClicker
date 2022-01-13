badfa0d8510a0a33c9612348011a60619bfd443b
IanTheBean <ian@morganfamilymail.com>
2022-01-13 12:08:45 -0600

    cleaned up app css

--- cookie-clicker/src/Components/App/App.js
+++ cookie-clicker/src/Components/App/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="text">Hello world</div>
+    </div>
+  );
 }
 
 export default App;
